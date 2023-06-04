9905354f5ea7417a77416fae5b8d4b23c0be84f1
Nicholas Galante <nicholasgalante94@gmail.com>
2023-06-03 20:07:36 -0400

    commit tree 879d92caa95f80abb1efe1935dd89345154364ac

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: { maximum: 250 }
+   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+   validate :clickbait_title
+   
+   CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait_title
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
